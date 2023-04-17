350e2a4b09f31bd3bdbf97893842b90ca8cfafe9
Hieff <hieffhiuff@gmail.com>
2023-04-17 17:19:21 +0100

    Updating the recipe display view

--- FoodApp/RecipeViewController.swift
+++ FoodApp/RecipeViewController.swift
@@ -14,35 +14,67 @@ class RecipeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        titleText.text = selectedRecipe?.strMeal ?? "no title"
-        instructionText.text = selectedRecipe?.strInstructions ?? "no instruction"
-        if(selectedRecipe?.strImageSource != nil){
-            recipeImage.loadFrom(URLAddress: (selectedRecipe?.strImageSource)!)
-        }
-        timeText.text = "Time: " + String(cookingTime()) + " minutes"
+            titleText.text = selectedRecipe?.strMeal ?? "no title"
+            instructionText.text = selectedRecipe?.strInstructions ?? "no instruction"
+            print(selectedRecipe?.strMealThumb ?? "none")
+            if(selectedRecipe?.strImageSource != nil){
+                print("working")
+                recipeImage.loadFrom(URLAddress: (selectedRecipe?.strMealThumb)!)
+            }
+            var time = cookingTime()
+            if(time <= 0){
+                time = 60
+            }
+            timeText.text = "Time: ~" + String(Double(cookingTime()) * 1.4) + " minutes"
     }
     
+    
+    // Calculating time it takes to cook the recipe
     func cookingTime() -> Int{
         let instructionsArray = instructionText.text.components(separatedBy: " ")
-        print(instructionsArray)
         var timeCounter = 0
         var x = 0
         while x < instructionsArray.count{
-            if(instructionsArray[x].lowercased().contains("minutes") || instructionsArray[x].lowercased().contains("mins")){
+            if(instructionsArray[x].lowercased().contains("minutes") || instructionsArray[x].lowercased().contains("min")){
                 print(instructionsArray[x-1])
-                timeCounter += Int(instructionsArray[x-1]) ?? 0
+                if(instructionsArray[x-1].contains("-")){
+                    timeCounter += removingDash(input: instructionsArray[x-1])
+                } else {
+                    timeCounter += Int(instructionsArray[x-1]) ?? 0
+                }
             }
-            if(instructionsArray[x].lowercased().contains("seconds")){
+            if(instructionsArray[x].lowercased().contains("seconds") || instructionsArray[x].lowercased().contains("sec")){
                 print(instructionsArray[x-1])
-                timeCounter += (Int(instructionsArray[x-1]) ?? 0) / 60
+                if(instructionsArray[x-1].contains("-")){
+                    timeCounter += removingDash(input: instructionsArray[x-1])
+                } else {
+                    timeCounter += (Int(instructionsArray[x-1]) ?? 0) / 60
+                }
+                
             }
-            if(instructionsArray[x].lowercased().contains("hours")){
-                timeCounter += (Int(instructionsArray[x-1]) ?? 0) * 60
+            if(instructionsArray[x].lowercased().contains("hours") || instructionsArray[x].lowercased().contains("hr")){
+                print(instructionsArray[x-1])
+                if(instructionsArray[x-1].contains("-")){
+                    timeCounter += removingDash(input: instructionsArray[x-1])
+                } else {
+                    timeCounter += (Int(instructionsArray[x-1]) ?? 0) * 60
+                }
             }
             x = x + 1
         }
         return timeCounter
     }
+    
+    func removingDash(input:String) -> Int {
+        let number = input.firstIndex(of: "-")!
+        var changedString = input[number...]
+        changedString.remove(at: changedString.firstIndex(of: "-")!)
+        let integer = Int(changedString) ?? 0
+        print(integer)
+        return integer
+        
+    }
+
     
     @IBOutlet weak var recipeImage: UIImageView!
     @IBOutlet weak var starImage: UIImageView!
@@ -71,17 +103,21 @@ extension UIImageView {
         if let url = URL(string: URLAddress) {
              let session = URLSession.shared
             session.dataTask(with: url) { (data, response, err) in
-                
+                if let err = err {
+                    print("Error loading picture\(err)")
+                    return;
+                }
                 do {
+                    print("doing")
                     if let imageData = try? Data(contentsOf: url) {
                         if let loadedImage = UIImage(data: imageData) {
+                            print("worked")
                             pic = loadedImage
-                           
+                            
                         } else {return}
                     }
-                } catch let err {
-                    print("Error loading image", err)
                 }
+                    
                 DispatchQueue.main.async(){
                     self.image = pic
                 }
