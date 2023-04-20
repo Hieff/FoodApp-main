f229c24e805c7be6e6fe65f1b8ed0e19a6ea06f8
Hieff <hieffhiuff@gmail.com>
2023-04-20 16:02:32 +0100

    Added suggestions

--- FoodApp/ViewController.swift
+++ FoodApp/ViewController.swift
@@ -14,21 +14,23 @@ class ViewController: UIViewController, UITabBarDelegate {
     @IBOutlet weak var recentImage: UIImageView!
     @IBOutlet weak var recentText: UILabel!
     
+    @IBOutlet weak var suggestionOne: UIImageView!
     
+    @IBOutlet weak var suggestionTwo: UIImageView!
     
     
     let mealManager: FetchedMealManager = FetchedMealManager()
     let fatSecretManager: FatSecretManager = FatSecretManager()
+    var sendingRecipe: MealDb.Meals? = nil
+    var suggestions = [MealDb.Meals]()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         searchBarField.addTarget(self, action: #selector(onSearch), for: .editingDidEndOnExit)
 
-        
-      
-    
-        
+
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -43,13 +45,96 @@ class ViewController: UIViewController, UITabBarDelegate {
                         self.recentImage.image = img;
                     }
                 }
+               
             }
         }
+        if(mealManager.listOfMeals.count != 0){
+            getSuggestions()
+        }
+       
     }
     
     @objc func onSearch() {
         performSegue(withIdentifier: "SearchScene", sender: self);
     }
+    
+    @objc func sendOne(){
+        sendingRecipe = suggestions[0]
+        print("working")
+        performSegue(withIdentifier: "mainToRecipe", sender: nil)
+    }
+    
+    @objc func sendTwo(){
+        sendingRecipe = suggestions[1]
+        performSegue(withIdentifier: "mainToRecipe", sender: nil)
+    }
+    
+    
+    func getSuggestions(){
+        let reviewedRecipes = (UserDefaults.standard.array(forKey: "reviews") ?? [""]) as! [String]
+        let recipeRatings = (UserDefaults.standard.array(forKey: "ratings") ?? [""]) as! [String]
+        print(reviewedRecipes)
+        print(recipeRatings)
+        suggestions = [MealDb.Meals]()
+        let alphabet = "qwertyuioplkjhgfdsazxcvbnm"
+        if reviewedRecipes[0] == "" {
+            let letter = "\(String(describing: alphabet.randomElement()!))"
+            let randomSearch = mealManager.getMealsBySearch(name: letter)
+            if randomSearch.count != 0 {
+                suggestions.append(randomSearch.randomElement()!)
+                suggestions.append(randomSearch.randomElement()!)
+            }
+        } else {
+            var counter = 0
+            var biggestRating = 0
+            var location = 0
+            for x in recipeRatings {
+                if Int(x) ?? 0 >= biggestRating {
+                    location = counter
+                }
+                counter += 1
+            }
+            var category = ""
+            for (_, meal) in (mealManager.listOfMeals) {
+                if meal.strMeal == reviewedRecipes[location] {
+                    category = meal.strArea
+                }
+            }
+            
+            for (_, meal) in (mealManager.listOfMeals){
+                if meal.strArea == category{
+                    suggestions.append(meal)
+                }
+            }
+            
+            var randomSuggestions = [MealDb.Meals]()
+            randomSuggestions.append(suggestions.randomElement()!)
+            randomSuggestions.append(suggestions.randomElement()!)
+            suggestions = randomSuggestions
+            
+        }
+        
+        if suggestions.count != 0 {
+            ImageFinder().fetch(suggestions[0].strMealThumb!) { img in
+                DispatchQueue.main.async {
+                    self.suggestionOne.image = img;
+                }
+            }
+            ImageFinder().fetch(suggestions[1].strMealThumb!) { img in
+                DispatchQueue.main.async {
+                    self.suggestionTwo.image = img;
+                }
+            }
+        }
+            
+        suggestionOne.isUserInteractionEnabled = true
+        suggestionTwo.isUserInteractionEnabled = true
+        suggestionOne.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(sendOne)))
+        suggestionTwo.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(sendTwo)))
+    }
+    
+    
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue){}
     
     
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
@@ -95,6 +180,11 @@ class ViewController: UIViewController, UITabBarDelegate {
         if segue.identifier == "toIngredientSearch"{
             let ingredientSearchController = segue.destination as? IngredientSearchViewController
             ingredientSearchController?.mealManager = self.mealManager
+        }
+        
+        if segue.identifier == "mainToRecipe"{
+            let recipeSearchController = segue.destination as? RecipeViewController
+            recipeSearchController?.selectedRecipe = sendingRecipe
         }
     }
 }
